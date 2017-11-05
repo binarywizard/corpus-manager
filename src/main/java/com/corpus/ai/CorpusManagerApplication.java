2ca21e778cfaebf98509012333ae5c7fe3c14423
xiaosong <xiaosong@qiyi.com>
2017-11-05 11:18:54 +0800

    First commit.

---
+++ src/main/java/com/corpus/ai/CorpusManagerApplication.java
@@ -0,0 +1,12 @@
+package com.corpus.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpusManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpusManagerApplication.class, args);
+	}
+}
